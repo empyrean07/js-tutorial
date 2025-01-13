0dffdfc010fffd38a12907455dc6ddf7a2cbbb73
Akash <141480081+empyrean07@users.noreply.github.com>
2025-01-13 11:43:29 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; //Treats all the code written below in the newer version of the JS 
+// alert(3+3) // here we are using node for the execution of the code so this will not work this only works on the browser
+console.log(3
+    +
+    3
+); // This decreases the readiablity of the code and considered as bad practice for writeing code
+
+console.log("Akash"); console.log("Age is 19") // This is also a bad practice for the coding 
+
+ let name="akash"
+ let age=19
+ let isLoggedIn=false
+ let state;
+ 
+/*
+premative data types
+// numbers => 2 to power 53
+// bigInt
+// string  => " "
+// boolean => true/ false
+// null => standalone value
+// undefined =>
+// symbol => unique
+*/
+
+/*
+Non-premative data types
+object
+*/
+
+console.log(typeof "Akash"); // output => String
+console.log(typeof null); // output => object
+console.log(typeof undefined); // output => undefined
