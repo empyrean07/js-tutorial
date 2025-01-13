2b3237f9ea7858b9cdbcd4bffdb33e3ccad38706
Akash <141480081+empyrean07@users.noreply.github.com>
2025-01-13 14:13:46 +0000

    understanding operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,10 +1,10 @@
 let score ="33"
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let scoreNumber = Number(score)
-console.log(typeof scoreNumber);
-console.log(scoreNumber); // output => NaN not a nuber is the fullform for this word
+// console.log(typeof scoreNumber);
+// console.log(scoreNumber); // output => NaN not a nuber is the fullform for this word
 
 // "33" => 33
 // "33abcI' => NaN
@@ -12,8 +12,8 @@ console.log(scoreNumber); // output => NaN not a nuber is the fullform for this 
 
 let isLoggedIn = "Akash"
 let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn);
-console.log(typeof(booleanisLoggedIn));
+// console.log(booleanisLoggedIn);
+// console.log(typeof(booleanisLoggedIn));
 
 
 // 1 => true; => false
@@ -23,6 +23,32 @@ console.log(typeof(booleanisLoggedIn));
 
 let someNumber = 123456
 let stringNUmber= String(someNumber)
-console.log(stringNUmber);
-console.log(typeof (stringNUmber));
+// console.log(stringNUmber);
+// console.log(typeof (stringNUmber));
 
+
+// *********************OPERATIONS*********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(4-1);
+// console.log(4*3);
+// console.log(2**4);
+// console.log(4/1);
+// console.log(2%3);
+
+let str1 = "Akash"
+let str2 = " Kumar"
+// console.log(str1 + str2)
+ 
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+3)
+// console.log(1+2+"3")
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
